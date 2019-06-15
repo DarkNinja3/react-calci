119ab7d2cec18421997191d03a6fb9c3397d21fe
asad <asadrajput2@gmail.com>
2019-06-15 22:46:42 +0530

    Updates

---
+++ precache-manifest.6ec3259acaca90c3a8e0e00f91a5da54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33e14e293a568116c2e83b25be2cd2d",
+    "url": "/react-calci/index.html"
+  },
+  {
+    "revision": "396efd15ee284d28e181",
+    "url": "/react-calci/static/css/main.588f7cc3.chunk.css"
+  },
+  {
+    "revision": "a4bfaf8c4bccdd0a2cf9",
+    "url": "/react-calci/static/js/2.0771dcc1.chunk.js"
+  },
+  {
+    "revision": "396efd15ee284d28e181",
+    "url": "/react-calci/static/js/main.76578adf.chunk.js"
+  },
+  {
+    "revision": "a67352361e8f334c34c1",
+    "url": "/react-calci/static/js/runtime~main.bd7e00f5.js"
+  }
+]);
